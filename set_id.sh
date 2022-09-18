7f9fd19172d22c50c802ff08f92344911e301c33
Yitao Yuan <yuanyitao@pku.edu.cn>
2022-09-18 18:05:01 +0800

    add set_id.sh

--- set_id.sh
+++ set_id.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 LAN_ADDR=192.168.0
 WAN_ADDR=192.168.2
@@ -12,7 +12,7 @@ REMOTE_NET_ADDR=
 
 if [ '(' "$ID" -ge 0 -a "$ID" -le 9 \) ] # '(' and \( is the same
 then
-	echo "ID=$ID"
+	: # pass
 else 
 	echo "invalid ID"
 	exit
@@ -31,14 +31,36 @@ else
 	exit
 fi
 
-sudo ip addr add $LOCAL_NET_ADDR.$ID dev $IF
+(
+set -x
+sudo ip addr replace $LOCAL_NET_ADDR.$ID dev $IF #replace = add or change
 sudo ip link set enp178s0f0 up
+)
 
-sudo route del -net $LOCAL_NET_ADDR.0/24
-sudo route del -net $REMOTE_NET_ADDR.0/24
+set +e #grep will return 1 on mismatch
+route1=`route | grep -E "$LOCAL_NET_ADDR\.0(\s+)0\.0\.0\.0(\s+)255\.255\.255\.0"`
+route2=`route | grep -E "$REMOTE_NET_ADDR\.0(\s+)$LOCAL_NET_ADDR\.254(\s+)255\.255\.255\.0"`
+set -e
 
-sudo route add -net $LOCAL_NET_ADDR.0/24 dev $IF
-sudo route add -net $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254
+if [ -z "$route1" ]
+then 
+	(
+	set -x
+	sudo route add -net $LOCAL_NET_ADDR.0/24 dev $IF
+	)
+else
+	echo "Already have route entry: $LOCAL_NET_ADDR.0/24"
+fi
+
+if [ -z "$route2" ]
+then
+	(
+	set -x
+	sudo route add -net $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254
+	)
+else
+	echo "Already have route entry: $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254"
+fi
 
 MAC_PREFIX=
 if [ $1 == "LAN" ]
@@ -49,4 +71,7 @@ else
 fi
 MAC_TAIL=0$ID
 
+(
+set -x
 sudo sudo arp -i enp178s0f0 -s $LOCAL_NET_ADDR.254 $MAC_PREFIX:$MAC_TAIL
+)
