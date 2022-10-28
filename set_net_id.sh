162b5407d4b650e575167f3e9fb4a4fecc8e8c63
Yitao Yuan <yuanyitao@pku.edu.cn>
2022-10-28 12:49:49 +0800

    update set_net_id.sh

--- set_net_id.sh
+++ set_net_id.sh
@@ -10,6 +10,8 @@ ID=$2
 LOCAL_NET_ADDR=
 REMOTE_NET_ADDR=
 
+MAC_PREFIX=01:23:45:67:89
+
 if [ '(' "$ID" -ge 0 -a "$ID" -le 9 \) ] # '(' and \( is the same
 then
 	: # pass
@@ -35,45 +37,64 @@ fi
 
 (
 set -x
-sudo ip link set $IF down
+sudo ip link set $IF down # this will clear route && arp entries related to $IF
 sudo ifconfig $IF $LOCAL_NET_ADDR.$ID netmask 255.255.255.0
-#sudo ip addr replace $LOCAL_NET_ADDR.$ID dev $IF #replace = add or change
-sudo ip link set $IF up
+sudo ip link set $IF up 
 )
 
 set +e #grep will return 1 on mismatch
-route1=`route | grep -E "$LOCAL_NET_ADDR\.0(\s+)0\.0\.0\.0(\s+)255\.255\.255\.0"`
-route2=`route | grep -E "$REMOTE_NET_ADDR\.0(\s+)$LOCAL_NET_ADDR\.254(\s+)255\.255\.255\.0"`
+route1=`route | grep -E "$LOCAL_NET_ADDR\.0"`
+route2=`route | grep -E "$REMOTE_NET_ADDR\.0"`
+arp1=`arp | grep -E "$LOCAL_NET_ADDR\.254"`
+arp2=`arp | grep -E "$REMOTE_NET_ADDR\.254"`
 set -e
 
-if [ -z "$route1" ]
+if [ -n "$route1" ]
 then 
-	(
-	set -x
-	sudo route add -net $LOCAL_NET_ADDR.0/24 dev $IF
-	)
-else
-	echo "Already have route entry: $LOCAL_NET_ADDR.0/24"
+    (
+    set -x 
+    sudo route del -net $LOCAL_NET_ADDR.0/24
+    )
 fi
 
-if [ -z "$route2" ]
+if [ -n "$route2" ]
 then
-	(
-	set -x
-	sudo route add -net $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254
-	)
-else
-	echo "Already have route entry: $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254"
+    (
+    set -x
+    sudo route del -net $REMOTE_NET_ADDR.0/24
+    )
 fi
 
-MAC_PREFIX=01:23:45:67:89
+if [ -n "$arp1" ]
+then
+    (
+    set -x 
+    sudo arp -i $IF -d $LOCAL_NET_ADDR.254
+    )
+fi
+
+if [ -n "$arp2" ]
+then
+    (
+    set -x
+    sudo arp -i $IF -d $REMOTE_NET_ADDR.254
+    )
+fi
+	
+(
+set -x
+sudo route add -net $LOCAL_NET_ADDR.0/24 dev $IF
+)
+
+if [ "$1" == "LAN" ]
+then
+    (
+    set -x
+    sudo route add -net $REMOTE_NET_ADDR.0/24 gw $LOCAL_NET_ADDR.254
+    )
+fi
 
 (
 set -x
 sudo arp -i $IF -s $LOCAL_NET_ADDR.254 $MAC_PREFIX:$MAC_TAIL
 )
-
-#(
-#set -x
-#sudo ethtool -K $IF tx off 
-#)
